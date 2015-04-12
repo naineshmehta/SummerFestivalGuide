36800f33760426a93f0cda423319763259afb77b
Nainesh Mehta <nainesh@cbconline.co.nz>
2015-04-12 18:48:34 +0100

    Latest News Article update, contentArea update to medium cols

--- Portals/_default/Skins/DnnFoundation/js/sfg.js
+++ Portals/_default/Skins/DnnFoundation/js/sfg.js
@@ -1,5 +1,6 @@
 ﻿$(document).ready(function () {
-    $('div.DnnModule-562, div.DnnModule-571, div.DnnModule-541').addClass('show-for-large-up');
+    $('div.DnnModule-562').addClass('show-for-large-up');
+    $('div.DnnModule-571,div.DnnModule-541').addClass('show-for-medium-up');
 
     $('div.DnnModule-646 img').css('width', 'auto').css('height', 'auto');
 });
